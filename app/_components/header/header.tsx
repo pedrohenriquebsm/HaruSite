a148784ee80be370ecc00bf3f036b8b1bbdf39c5
pedrohenriquebsm <phpedrohenrique474@outlook.com>
2026-09-12 00:17:50 -0300

    fixed header & added logo to the landing page

--- app/_components/header/header.tsx
+++ app/_components/header/header.tsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react";
 
 export default function Header() {
   return (
-    <header>
+    <header className="sticky top-0">
       <nav className="navbar p-5 px-10 flex justify-between md:justify-around flex-wrap">
         <Link href={'/'} className="font-[Cormorant_Garamond] text-xl tracking-widest font-bold p-2">HARU</Link>
 
@@ -14,7 +14,7 @@ export default function Header() {
         <span className="md:hidden navbar-end">
           <details className="dropdown">
             <summary className="btn btn-ghost active:bg-transparent focus:bg-transparent focus:border-0 hover:bg-transparent hover:border-0"><Menu/></summary>
-            <ul className="text-xs tracking-widest menu dropdown-content fixed left-0 w-screen gap-2 rounded-none z-1 shadow-sm p-2">
+            <ul className="text-xs bg-[#f6f1e8] tracking-widest menu dropdown-content fixed left-0 w-screen gap-2 rounded-none z-1 shadow-sm p-2">
               <HeaderNavButtons />
             </ul>
           </details>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import Header from "./_components/header/header";
 
 export default function Home() {
   return (
     <>
-    <Header></Header>
+      <Header />
+      <div className="flex justify-center items-center min-h-screen">
+        <Image
+          src="/logos/logo-light.png"
+          alt="Logo da Haru, sendo um desenho minimalista de uma silhueta de um cavalo"
+          width={587}
+          height={695}
+          loading="eager"
+          className="w-auto h-auto"
+        />
+      </div>
     </>
   );
 }
