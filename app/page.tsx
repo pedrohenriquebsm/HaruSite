e0c06f1c8b79e6e5e532faa008de8cf1fde3de9b
pedrohenriquebsm <phpedrohenrique474@outlook.com>
2026-09-18 09:07:23 -0300

    homepage improvements

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,29 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex justify-center items-center min-h-screen">
-        <Image
-          src="/logos/logo-light.png"
-          alt="Logo da Haru, sendo um desenho minimalista de uma silhueta de um cavalo"
-          width={587}
-          height={695}
-          loading="eager"
-          className="w-auto h-auto"
-        />
-      </div>
+      <main>
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          <Image
+            src="/logos/logo-light.png"
+            alt="Logo da Haru, sendo um desenho minimalista de uma silhueta de um cavalo"
+            width={587}
+            height={695}
+            loading="eager"
+            className="w-38 h-auto"
+          />
+          <h1 className="font-[Cormorant_Garamond] text-center text-8xl tracking-[10] font-medium">HARU</h1>
+        </div>
+        <div className="-z-1">
+            <Image
+              src="/background/stale-grut-NUgw97CVdAk-unsplash.jpg"
+              alt="Imagem de uma plantação de bambu"
+              width={1920}
+              height={1280}
+              loading="eager"
+              className="w-screen h-auto"
+            />
+          </div>
+      </main>
     </>
   );
 }
