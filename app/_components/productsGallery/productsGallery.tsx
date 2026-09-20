a34c8feb200f1bd2395bf41133277f2cb1f01963
pedrohenriquebsm <phpedrohenrique474@outlook.com>
2026-09-20 17:06:45 -0300

    feat: products list component

---
+++ app/_components/productsGallery/productsGallery.tsx
@@ -0,0 +1,46 @@
+import Product from "../product/product";
+
+export default function ProductsGallery() {
+  return (
+    <section className="md:mt-10 p-10">
+      <span className="flex flex-col gap-10 py-15">
+        <p className="tracking-widest text-sm font-light">COLEÇÃO</p>
+        <p className="font-[Cormorant_Garamond_Variable] text-5xl font-medium">
+          O essencial.
+        </p>
+      </span>
+      <span className="grid place-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4  gap-10">
+        <Product
+          description="Cabo de bambu selecionado, sem verniz plástico. esterilizadas a vapor."
+          name="Escova de bambu"
+          price={48.99}
+          id="10"
+        />
+        <Product
+          description="Cabo de bambu selecionado, sem verniz plástico. esterilizadas a vapor."
+          name="Escova de bambu"
+          price={48.99}
+          id="10"
+        />
+        <Product
+          description="Cabo de bambu selecionado, sem verniz plástico. esterilizadas a vapor."
+          name="Escova de bambu"
+          price={48.99}
+          id="10"
+        />
+        <Product
+          description="Cabo de bambu selecionado, sem verniz plástico. esterilizadas a vapor."
+          name="Escova de bambu"
+          price={48.99}
+          id="10"
+        />
+        <Product
+          description="Cabo de bambu selecionado, sem verniz plástico. esterilizadas a vapor."
+          name="Escova de bambu"
+          price={48.99}
+          id="10"
+        />
+      </span>
+    </section>
+  );
+}
